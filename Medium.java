9da0e17c18b53defaa57ddbb103a822e211ea1e9
Kirsten <>
2020-10-11 15:29:06 -0500

    stars

---
+++ Medium.java
@@ -0,0 +1,24 @@
+// EASY ANSWER
+// C
+// 
+public class Medium {
+
+    public static void main(String[] args) {
+        String[] simpleArray = {"*", "**", "***", "****", "*****"};
+ 
+// using the standard for loop and the length property of an array
+for (int i = 0; i < simpleArray.length; i++) 
+{
+    System.out.println(simpleArray[i]);
+}
+ 
+// using the enhanced for loop
+// for (int item : simpleArray) 
+// {
+//     System.out.println("Enhanced for: " + item);
+// }
+ 
+// both loops will print the their respective strings 1 - 10. 
+
+}
+}
